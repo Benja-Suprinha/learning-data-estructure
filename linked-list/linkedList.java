6dc369ab824c6cb99dfcfac11e356e0086a2272f
benjamin <benjamin@valuelist.cl>
2024-01-04 16:35:52 -0300

    Fix linkList class and node class

--- linked-list/linkedList.java
+++ linked-list/linkedList.java
@@ -1,39 +1,36 @@
 public class linkedList {
     public class linkList {
         public node head;
-        public node next;
 
-        linkList(node head, node next) {
-            this.head = head;
-            if (next == null) {
-                this.next = null;
-            } else {
-                this.next = next;
-            }
+        linkList() {
+            this.head = null;
         }
 
-        public void addNode(node newNode) {
-            if (this.next == null) {
-                this.next = newNode;
+        public void addNode(linkList l, int data) {
+            node node = new node(data);
+            if (l.head == null) {
+                l.head = node;
+            } else {
+                node temp = l.head;
+                while (temp.next != null) {
+                    temp = temp.next;
+                }
+                temp.next = node;
             }
         }
     }
 
     public class node {
         public int data;
+        public node next;
 
         node(int data) {
             this.data = data;
+            this.next = null;
         }
     }
 
     public static void main(String[] args) {
         // Your code here
-        linkedList list = new linkedList();
-
-        node node1 = list.new node(1);
-
-        linkList list1 = list.new linkList(node1, null);
-        System.out.println(list1.head.data);
     }
 }
