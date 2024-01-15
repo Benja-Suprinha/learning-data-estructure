6dace015e8af5b8ce3220e53044a14049badb9bc
Benjamin Suprinha <benjamin.ojeda@mail.udp.cl>
2024-01-15 11:58:21 -0300

    Add delete last node

--- linked-list/linkedList.java
+++ linked-list/linkedList.java
@@ -2,7 +2,7 @@ public class linkedList {
     node head; //se define la cabeza de la lista
     int size = 0; //se define el tamaño de la lista
 
-    public static linkedList addNode(linkedList list, int data) {
+    public static linkedList addNodeEnd(linkedList list, int data) {
         node newNode = new node(data);
         newNode.next = null;
 
@@ -28,6 +28,21 @@ public class linkedList {
         }
     }
 
+    public static linkedList removeLastNode(linkedList list) {
+        node currNode = list.head;
+        if (list.head == null) {
+            System.out.println("Lista vacia");
+        } else {
+            while (currNode.next.next != null) {
+                currNode = currNode.next;
+            }
+            currNode.next = null;
+            list.size--;
+        }
+        
+        return list;
+    }
+
     static class node { //se define la clase nodo que contiene el dato y el apuntador de la lista
         public int data;
         public node next;
@@ -41,10 +56,14 @@ public class linkedList {
     public static void main(String[] args) {
         linkedList list = new linkedList();
 
-        list = addNode(list, 1);
-        list = addNode(list, 2);
-        list = addNode(list, 3);
+        list = addNodeEnd(list, 1);
+        list = addNodeEnd(list, 2);
+        list = addNodeEnd(list, 3);
 
+        printList(list);
+        System.out.println("\nSize: " + list.size);
+
+        list = removeLastNode(list);
         printList(list);
         System.out.println("\nSize: " + list.size);
     }
